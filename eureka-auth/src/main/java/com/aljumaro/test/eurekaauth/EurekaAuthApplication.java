bafcfdef8cd08d7823f64d553f916971ee1765e3
Juanma <aljumaro@gmail.com>
2017-10-15 11:51:08 +0200

    Authorization service

---
+++ eureka-auth/src/main/java/com/aljumaro/test/eurekaauth/EurekaAuthApplication.java
@@ -0,0 +1,12 @@
+package com.aljumaro.test.eurekaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaAuthApplication.class, args);
+	}
+}
